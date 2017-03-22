73289d3b2f558a1b2643c8dbebc0580a037da8cc
esbb48 <esbb48@gmail.com>
2017-03-22 15:30:45 +0800

    🐛 Fix: Fix Uncaught Error: Invariant Violation: Element type is invalid: expected a string problem

--- common/routes/News/components/index.js
+++ common/routes/News/components/index.js
@@ -1,9 +1,8 @@
-export { default as FontSize } from './FontSize';
-export { default as Head } from './Head';
-export { default as MainContent } from './MainContent';
-export { default as NewsContent } from './NewsContent';
-export { default as ReleatedContent } from './ReleatedContent';
-export { default as Social } from './Social';
-export { default as ThermometerSm } from './ThermometerSm';
-export { default as Thermometer } from './Thermometer';
+export Head from './Head';
+export FontSize from './FontSize';
+export NewsContent from './NewsContent';
+export ReleatedContent from './ReleatedContent';
+export Social from './Social';
+export ThermometerSm from './ThermometerSm';
+export Thermometer from './Thermometer';
 
--- common/routes/News/containers/NewsPage.js
+++ common/routes/News/containers/NewsPage.js
@@ -3,7 +3,12 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet, css } from 'aphrodite/no-important';
 import { loadNews, selectCurrentNews } from '../../../modules/news';
-import { Head, MainContent } from '../components';
+import {
+  Head, FontSize, NewsContent, Social, Thermometer,
+  ThermometerSm, ReleatedContent
+} from '../components';
+
+import { Ad300x250 } from '../../../components/Ad';
 
 const redial = {
   fetch: ({ dispatch, params: { id } }) => dispatch(loadNews(`news/${id}`))
@@ -22,7 +27,27 @@ const NewsPage = ({data, isLoading}) => {
       {!isLoading &&
         <div>
           <Head newsBy={newsBy} mainMenu={MainMenu} title={title} />
-          <MainContent news={news} />
+          <div className={css(styles.box)}>
+            <img className={css(styles.img)} src={news.MainPhoto && news.MainPhoto.url || 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg'} />
+            <span>{news.MainPhoto && news.MainPhoto.desc}</span>
+            <div className={css(styles.content)}>
+              <div className={css(styles.leftSide)}>
+                <NewsContent content={news.content} />
+                <Social />
+                <ThermometerSm />
+                <ReleatedContent type='相關新聞' />
+                <ReleatedContent type='你可能會喜歡' />
+              </div>
+              <div className={css(styles.rightSide)}>
+                <Social />
+                <FontSize />
+                <Ad300x250 />
+                <Thermometer />
+                <Ad300x250 />
+                <Ad300x250 />
+              </div>
+            </div>
+          </div>
         </div>}
     </div>
   );
@@ -34,11 +59,23 @@ NewsPage.propTypes = {
 };
 
 const styles = StyleSheet.create({
+  box: {
+    width: 900,
+    margin: '0 auto'
+  },
   content: {
-    fontSize: '1rem',
-    lineHeight: '1.5',
-    margin: '1rem 0',
-    color: '#555'
+    display: 'inline-flex',
+    margin: '1rem 0'
+  },
+  img: {
+    height: '100%',
+    width: '100%'
+  },
+  leftSide: {
+    width: '69%'
+  },
+  rightSide: {
+    width: '31%'
   },
   title: {
     fontSize: 28,
