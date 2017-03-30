c247af291c2e375e54edddfa191d29568705b220
WebberWu <bird23074035@gmail.com>
2017-03-30 23:13:57 +0800

    ✨ Feat: add style home

--- common/routes/Home/containers/HomePage.js
+++ common/routes/Home/containers/HomePage.js
@@ -2,9 +2,12 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { provideHooks } from 'redial';
 import { StyleSheet, css } from 'aphrodite/no-important';
-import moment from 'moment';
+// import moment from 'moment';
 import Header from '../../../components/Header';
-import ListItem from '../../../components/News/ListItem';
+import BlockItems from '../components/BlockItems';
+import { Container, RightSide, LeftSide, ClearFix } from '../../../components/Layout';
+import { Ad300x250, Ad300x600 } from '../../../components/Ad';
+
 import { loadMenus, selectMenus } from '../../../modules/menus';
 import { selectHomePage, loadHomeList } from '../module';
 
@@ -21,10 +24,32 @@ const mapStateToProps = state => ({
 });
 
 const HomePage = ({ menus, homePage }) => (
-  <div>
+  <Container>
     <Header menus={menus} />
     <div> 首頁輪播 </div>
     <div className={css(styles.body)}>首頁內容</div>
+    <LeftSide>
+      {homePage.isLoading &&
+        <div>
+          <h2>Loading ...</h2>
+        </div>}
+      {!homePage.isLoading && homePage.data.length === 0 &&
+        <div>查無相關新聞 ... </div>}
+      {!homePage.isLoading && homePage.data.length > 0 &&
+        <div className={css(styles.blockItem)}>
+          <BlockItems newsList={homePage.data.slice(0, 4)} />
+        </div>
+      }
+      <div className={css(styles.seeMoreBlock)}>
+        <span className={css(styles.seeMoreText)}>看更多專題</span>
+      </div>
+    </LeftSide>
+    <RightSide>
+      <Ad300x250 />
+      <Ad300x250 />
+    </RightSide>
+    <ClearFix />
+    <hr />
     {homePage.isLoading &&
       <div>
         <h2>Loading ...</h2>
@@ -32,24 +57,46 @@ const HomePage = ({ menus, homePage }) => (
     {!homePage.isLoading && homePage.data.length === 0 &&
       <div>查無相關新聞 ... </div>}
     {!homePage.isLoading && homePage.data.length > 0 &&
-      <div>
-        {homePage.data.map((value, i) => (
-          <ListItem
-            key={value.sn}
-            category={value.MainMenu && value.MainMenu.name || 'Sponsored'}
-            photo={value.MainPhoto}
-            title={value.shortTitle}
-            time={moment(value.formatStartedAt).format('YYYY/MM/DD')}
-            url={`/news/${moment(value.formatStartedAt).format('YYYYMMDD')}/${value.sn}`} />
-        ))}
+      <div className={css(styles.blockItem)}>
+        <BlockItems newsList={homePage.data.slice(0, 3)} />
+        <BlockItems newsList={homePage.data.slice(0, 3)} />
+        <BlockItems newsList={homePage.data.slice(0, 3)} />
       </div>
     }
-  </div>
+    <ClearFix />
+    <div className={css(styles.seeMoreBlock)}>
+      <span className={css(styles.seeMoreText)}>看更多地區新聞</span>
+    </div>
+    <div>影音區塊</div>
+    <LeftSide>
+      <BlockItems newsList={homePage.data.slice(0, 4)} />
+      <BlockItems newsList={homePage.data.slice(0, 4)} />
+    </LeftSide>
+    <RightSide>
+      <Ad300x600 />
+      <Ad300x250 />
+    </RightSide>
+    <ClearFix />
+  </Container>
 );
 
 const styles = StyleSheet.create({
   body: {
     height: 300
+  },
+  blockItem: {
+
+  },
+  seeMoreBlock: {
+    textAlign: 'center',
+    padding: '25px 0'
+  },
+  seeMoreText: {
+    color: '#222',
+    border: '1px solid #222',
+    padding: '6px 17px',
+    borderRadius: 30,
+    fontSize: 22
   }
 });
 
