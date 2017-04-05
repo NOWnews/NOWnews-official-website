9b8e99999e5cdced24778669b759420868077a65
WebberWu <bird23074035@gmail.com>
2017-04-05 22:35:44 +0800

    🎨 Style: style adjust

--- common/components/News/BlockItem.js
+++ common/components/News/BlockItem.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     fontSize: '13px'
   },
   img: {
-    height: 'auto',
+    height: '168px',
     marginBottom: 5,
     maxWidth: '100%',
     maxHeight: '168px',
