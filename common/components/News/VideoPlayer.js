73ae9a178a98c83db91862d916818f9b4205138d
esbb48 <esbb48@gmail.com>
2017-05-12 10:49:40 +0800

    🎨 Style: Update video player style

--- common/components/News/VideoPlayer.js
+++ common/components/News/VideoPlayer.js
@@ -39,8 +39,25 @@ class VideoPlayer extends Component {
   // see https://github.com/videojs/video.js/pull/3856
   render () {
     const { width = 970, height = 545 } = this.props;
+    const iconSize = 100;
+    const fontSize = 70;
+    const customVideoJsPlayer = `
+      .video-js .vjs-big-play-button {
+        border: 0;
+        border-radius: 50%;
+        left: ${width / 2 - iconSize / 2}px;
+        top: ${height / 2 - iconSize / 2}px;
+        width: ${iconSize}px;
+        height: ${iconSize}px;
+      }
+      .video-js .vjs-big-play-button:before {
+        padding-top: ${(iconSize - fontSize) / 2 + 10}px;
+        font-size: ${fontSize}px;
+      }
+    `;
     return (
       <div data-vjs-player>
+        <style dangerouslySetInnerHTML={{__html: customVideoJsPlayer}} />
         <video width={width} height={height} className='video-js'
           ref={node => { this.videoNode = node; }} />
       </div>
