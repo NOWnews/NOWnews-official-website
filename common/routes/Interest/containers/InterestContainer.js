172972fc7e764cee41316a01d3050c275c3b7015
esbb48 <esbb48@gmail.com>
2017-05-25 18:43:38 +0800

    ✨ Feat: Adjust interest page

--- common/routes/Interest/containers/InterestContainer.js
+++ common/routes/Interest/containers/InterestContainer.js
@@ -1,45 +1,41 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { provideHooks } from 'redial';
-import { selectInterestPage, loadInterestList } from '../module';
-
 import { loadHeader, selectMarquee, selectMenus } from '../../../modules/header';
+import { selectInterest, loadInterest } from '../../../modules/interest';
 
 import { Header } from '../../../components/Header';
 import { BlockItems12, TripletHead } from '../../../components/News';
 import { Container, Loading, NotFound } from '../../../components/Layout';
 
 const redial = {
-  fetch: ({ dispatch, query: { page } }) => Promise.all([
-    dispatch(loadInterestList(page)),
+  fetch: ({ dispatch }) => Promise.all([
+    dispatch(loadInterest()),
     dispatch(loadHeader())
   ])
 };
 
 const mapStateToProps = state => ({
-  interestPage: selectInterestPage(state),
+  interest: selectInterest(state),
   marquee: selectMarquee(state),
   menus: selectMenus(state)
 });
 
-const InterestContainer = ({ marquee, menus, interestPage }) => (
+const InterestContainer = ({ marquee, menus, interest }) => (
   <div>
     <Header menus={menus} marquee={marquee} />
     <TripletHead active='interest' />
     <Container>
-
-      {interestPage.isLoading && <Loading />}
-      <h3>尚未開放，敬請期待！</h3>
-      {false && !interestPage.isLoading && interestPage.newsList.length === 0 && <NotFound />}
-      {!interestPage.isLoading && interestPage.newsList.length > 0 &&
-        <BlockItems12 hasAd={false} newsList={interestPage.newsList} page={interestPage.pageData} />
-      }
+      {interest.isLoading && <Loading />}
+      {!interest.isLoading && interest.newsList.length === 0 && <NotFound />}
+      {!interest.isLoading && interest.newsList.length > 0 &&
+        <BlockItems12 hasAd={false} newsList={interest.newsList} />}
     </Container>
   </div>
 );
 
 InterestContainer.propTypes = {
-  interestPage: PropTypes.object.isRequired,
+  interest: PropTypes.object.isRequired,
   marquee: PropTypes.array.isRequired,
   menus: PropTypes.array.isRequired
 };
