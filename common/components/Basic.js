54e5ce162069d0cd75c6698a209c7cd5a8906240
esbb48 <esbb48@gmail.com>
2017-05-31 02:34:41 +0800

    ✨ Feat: Add basic App for helmet and gtm
    it's no include footer

---
+++ common/components/Basic.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import Helmet from 'react-helmet';
+import { GTM } from './Tracking';
+
+const styles = StyleSheet.create({
+  root: {
+    color: '#000'
+  }
+});
+
+const Basic = ({ children }) => (
+  <div className={css(styles.root)}>
+    <Helmet title='NOWnews 今日新聞' titleTemplate='NOWnews 今日新聞'
+      link={[
+        {async: '', rel: 'stylesheet', href: '/vendor/basscss.min.css'},
+        {async: '', rel: 'stylesheet', href: '/vendor/font-awesome-4.7.0/css/font-awesome.min.css'},
+        {async: '', rel: 'stylesheet', href: '/vendor/carousel.min.css'},
+        {async: '', rel: 'stylesheet', href: '/vendor/video-js.min.css'}
+      ]}
+    />
+    {children}
+    <GTM gtmId='GTM-W25KLJG' />
+  </div>
+);
+
+Basic.propTypes = {
+  children: PropTypes.shape().isRequired
+};
+export default Basic;
