70843929d20507e23798c9ba7b2b1f4de91a26bc
esbb48 <esbb48@gmail.com>
2017-06-12 12:00:34 +0800

    📚 Docs: Add dpf component format

--- common/components/Ad/DFP.js
+++ common/components/Ad/DFP.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import Dfp from 'react-simple-dfp';
 
+// opts format: ['/5799246/Nownews_home_300x250_M1_new2', [300, 250], 'div-gpt-ad-1496983171426-0']
 const DFP = ({ className = '', opts }) => (
   <div className={className} >
     <Dfp adUnitPath={opts[0]}
