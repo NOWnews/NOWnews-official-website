419b33e74e5a30db47263defae20330865532549
esbb48 <esbb48@gmail.com>
2017-06-16 17:23:09 +0800

    ✨ Feat: Hidden account, pwd login method

--- common/routes/Auth/module.js
+++ common/routes/Auth/module.js
@@ -1,21 +1,21 @@
 import isomorphicCookie from 'isomorphic-cookie';
 import { CLEAR_USER } from '../../modules/sourceRequest';
-export const ACTIVE_REQUEST = 'ACTIVE_REQUEST';
-export const ACTIVE_SUCCESS = 'ACTIVE_SUCCESS';
-export const ACTIVE_FAILURE = 'ACTIVE_FAILURE';
+// export const ACTIVE_REQUEST = 'ACTIVE_REQUEST';
+// export const ACTIVE_SUCCESS = 'ACTIVE_SUCCESS';
+// export const ACTIVE_FAILURE = 'ACTIVE_FAILURE';
 export const LOAD_USER_REQUEST = 'LOAD_USER_REQUEST';
 export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS';
 export const LOAD_USER_FAILURE = 'LOAD_USER_FAILURE';
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+// export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+// export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+// export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
-export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
-export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
-export const RESEND_ACTIVE_REQUEST = 'RESEND_ACTIVE_REQUEST';
-export const RESEND_ACTIVE_SUCCESS = 'RESEND_ACTIVE_SUCCESS';
-export const RESEND_ACTIVE_FAILURE = 'RESEND_ACTIVE_FAILURE';
+// export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+// export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+// export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+// export const RESEND_ACTIVE_REQUEST = 'RESEND_ACTIVE_REQUEST';
+// export const RESEND_ACTIVE_SUCCESS = 'RESEND_ACTIVE_SUCCESS';
+// export const RESEND_ACTIVE_FAILURE = 'RESEND_ACTIVE_FAILURE';
 export const UPDATE_REQUEST = 'UPDATE_REQUEST';
 export const UPDATE_SUCCESS = 'UPDATE_SUCCESS';
 export const UPDATE_FAILURE = 'UPDATE_FAILURE';
@@ -27,28 +27,28 @@ const initialState = {
   user: null
 };
 
-export const activeEmail = (token) => {
-  return (dispatch, getState, { axios }) => {
-    dispatch({ type: ACTIVE_REQUEST });
-    const { memberServ } = getState().sourceRequest;
-    const url = `${memberServ}/api/auth/active?token=${token}`;
-    return axios.get(url)
-    .then((result) => {
-      dispatch({
-        type: ACTIVE_SUCCESS,
-        payload: result,
-        meta: {
-          lastFetched: Date.now()
-        }
-      });
-    }).catch(error => {
-      dispatch({
-        type: ACTIVE_FAILURE,
-        payload: error.response.data
-      });
-    });
-  };
-};
+// export const activeEmail = (token) => {
+//   return (dispatch, getState, { axios }) => {
+//     dispatch({ type: ACTIVE_REQUEST });
+//     const { memberServ } = getState().sourceRequest;
+//     const url = `${memberServ}/api/auth/active?token=${token}`;
+//     return axios.get(url)
+//     .then((result) => {
+//       dispatch({
+//         type: ACTIVE_SUCCESS,
+//         payload: result,
+//         meta: {
+//           lastFetched: Date.now()
+//         }
+//       });
+//     }).catch(error => {
+//       dispatch({
+//         type: ACTIVE_FAILURE,
+//         payload: error.response.data
+//       });
+//     });
+//   };
+// };
 
 // SSR render will use this function, so dont use win
 export const loadUser = () => {
@@ -82,33 +82,33 @@ export const loadUser = () => {
   };
 };
 
-export const onLogin = () => {
-  return (dispatch, getState, { axios }) => {
-    const { memberServ } = getState().sourceRequest;
-    const values = getState().form.auth.values;
-    dispatch({ type: LOGIN_REQUEST });
-    const url = `${memberServ}/api/member/signin`;
-    return axios.post(url, values)
-    .then((result) => {
-      const { id, ...user } = result.data;
-      isomorphicCookie.save('NOW_member', id, { secure: false });
-      isomorphicCookie.save('NOW_memberData', { ...user }, { secure: false });
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: result,
-        meta: {
-          lastFetched: Date.now()
-        }
-      });
-      window.location = '/';
-    }).catch(error => {
-      dispatch({
-        type: LOGIN_FAILURE,
-        payload: error.response.data
-      });
-    });
-  };
-};
+// export const onLogin = () => {
+//   return (dispatch, getState, { axios }) => {
+//     const { memberServ } = getState().sourceRequest;
+//     const values = getState().form.auth.values;
+//     dispatch({ type: LOGIN_REQUEST });
+//     const url = `${memberServ}/api/member/signin`;
+//     return axios.post(url, values)
+//     .then((result) => {
+//       const { id, ...user } = result.data;
+//       isomorphicCookie.save('NOW_member', id, { secure: false });
+//       isomorphicCookie.save('NOW_memberData', { ...user }, { secure: false });
+//       dispatch({
+//         type: LOGIN_SUCCESS,
+//         payload: result,
+//         meta: {
+//           lastFetched: Date.now()
+//         }
+//       });
+//       window.location = '/';
+//     }).catch(error => {
+//       dispatch({
+//         type: LOGIN_FAILURE,
+//         payload: error.response.data
+//       });
+//     });
+//   };
+// };
 
 export const onLogout = () => {
   return (dispatch, getState, { axios }) => {
@@ -124,33 +124,34 @@ export const onLogout = () => {
     window.location = '/';
   };
 };
-export const onSignup = () => {
-  return (dispatch, getState, { axios }) => {
-    const { memberServ } = getState().sourceRequest;
-    const values = getState().form.auth.values;
-    dispatch({ type: SIGNUP_REQUEST });
-    const url = `${memberServ}/api/member/signup`;
-    return axios.post(url, values)
-    .then((result) => {
-      dispatch({
-        type: SIGNUP_SUCCESS,
-        payload: result,
-        meta: {
-          lastFetched: Date.now()
-        }
-      });
-      const confirmRes = window.confirm('【註冊完成】，請前往註冊信箱做帳號認證！');
-      if (confirmRes) {
-        window.location = '/auth/login';
-      }
-    }).catch(error => {
-      dispatch({
-        type: SIGNUP_FAILURE,
-        payload: error.response.data
-      });
-    });
-  };
-};
+
+// export const onSignup = () => {
+//   return (dispatch, getState, { axios }) => {
+//     const { memberServ } = getState().sourceRequest;
+//     const values = getState().form.auth.values;
+//     dispatch({ type: SIGNUP_REQUEST });
+//     const url = `${memberServ}/api/member/signup`;
+//     return axios.post(url, values)
+//     .then((result) => {
+//       dispatch({
+//         type: SIGNUP_SUCCESS,
+//         payload: result,
+//         meta: {
+//           lastFetched: Date.now()
+//         }
+//       });
+//       const confirmRes = window.confirm('【註冊完成】，請前往註冊信箱做帳號認證！');
+//       if (confirmRes) {
+//         window.location = '/auth/login';
+//       }
+//     }).catch(error => {
+//       dispatch({
+//         type: SIGNUP_FAILURE,
+//         payload: error.response.data
+//       });
+//     });
+//   };
+// };
 
 export const onUpdate = () => {
   return (dispatch, getState, { axios }) => {
@@ -180,48 +181,48 @@ export const onUpdate = () => {
   };
 };
 
-export const resendActiveEmail = () => {
-  return (dispatch, getState, { axios }) => {
-    dispatch({ type: RESEND_ACTIVE_REQUEST });
-    const { memberServ } = getState().sourceRequest;
-    const { email } = getState().form.auth.values || {};
+// export const resendActiveEmail = () => {
+//   return (dispatch, getState, { axios }) => {
+//     dispatch({ type: RESEND_ACTIVE_REQUEST });
+//     const { memberServ } = getState().sourceRequest;
+//     const { email } = getState().form.auth.values || {};
 
-    if (!email || (email && email.trim().length === 0)) {
-      return dispatch({
-        type: RESEND_ACTIVE_FAILURE,
-        payload: {message: '信箱格式有誤'}
-      });
-    }
+//     if (!email || (email && email.trim().length === 0)) {
+//       return dispatch({
+//         type: RESEND_ACTIVE_FAILURE,
+//         payload: {message: '信箱格式有誤'}
+//       });
+//     }
 
-    return axios.post(`${memberServ}/api/auth/resend`, { email })
-    .then((result) => {
-      dispatch({
-        type: RESEND_ACTIVE_SUCCESS,
-        payload: result,
-        meta: {
-          lastFetched: Date.now()
-        }
-      });
-      const confirmRes = window.confirm('【重寄確認信成功】，請前往信箱做帳號認證！');
-      if (confirmRes) {
-        window.location = '/auth/login';
-      }
-    }).catch(error => {
-      dispatch({
-        type: RESEND_ACTIVE_FAILURE,
-        payload: error.response.data
-      });
-    });
-  };
-};
+//     return axios.post(`${memberServ}/api/auth/resend`, { email })
+//     .then((result) => {
+//       dispatch({
+//         type: RESEND_ACTIVE_SUCCESS,
+//         payload: result,
+//         meta: {
+//           lastFetched: Date.now()
+//         }
+//       });
+//       const confirmRes = window.confirm('【重寄確認信成功】，請前往信箱做帳號認證！');
+//       if (confirmRes) {
+//         window.location = '/auth/login';
+//       }
+//     }).catch(error => {
+//       dispatch({
+//         type: RESEND_ACTIVE_FAILURE,
+//         payload: error.response.data
+//       });
+//     });
+//   };
+// };
 
 export default function authPage (state = initialState, action) {
   switch (action.type) {
-    case ACTIVE_FAILURE:
+    // case ACTIVE_FAILURE:
+    // case LOGIN_FAILURE:
+    // case SIGNUP_FAILURE:
+    // case RESEND_ACTIVE_FAILURE:
     case LOAD_USER_FAILURE:
-    case LOGIN_FAILURE:
-    case SIGNUP_FAILURE:
-    case RESEND_ACTIVE_FAILURE:
     case UPDATE_FAILURE:
       return {
         ...state,
@@ -234,10 +235,10 @@ export default function authPage (state = initialState, action) {
         isLoading: false,
         user: action.payload
       };
-    case ACTIVE_SUCCESS:
-    case LOGIN_SUCCESS:
-    case SIGNUP_SUCCESS:
-    case RESEND_ACTIVE_SUCCESS:
+    // case ACTIVE_SUCCESS:
+    // case LOGIN_SUCCESS:
+    // case SIGNUP_SUCCESS:
+    // case RESEND_ACTIVE_SUCCESS:
     case UPDATE_SUCCESS:
       return {
         ...state,
@@ -249,14 +250,14 @@ export default function authPage (state = initialState, action) {
         isLoading: true,
         user: null
       };
-    case ACTIVE_REQUEST:
-    case LOGIN_REQUEST:
-    case SIGNUP_REQUEST:
-    case RESEND_ACTIVE_REQUEST:
-      return {
-        ...state,
-        isLoading: true
-      };
+    // case ACTIVE_REQUEST:
+    // case LOGIN_REQUEST:
+    // case SIGNUP_REQUEST:
+    // case RESEND_ACTIVE_REQUEST:
+      // return {
+      //   ...state,
+      //   isLoading: true
+      // };
     default:
       return state;
   }
