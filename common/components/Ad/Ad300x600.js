d31f205dbd876130893ec389833a9e0580f23682
esbb48 <esbb48@gmail.com>
2017-06-17 20:39:40 +0800

    ✨ Feat: Remove no need Ad

--- common/components/Ad/Ad300x600.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ad300x600 = () => (
-  <div>
-    <img src='http://fakeimg.pl/300x600/' />
-  </div>
-);
-
-export default Ad300x600;
--- common/components/Ad/Ad970x250.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ad970x250 = () => (
-  <div>
-    <img src='http://fakeimg.pl/970x250/' />
-  </div>
-);
-
-export default Ad970x250;
