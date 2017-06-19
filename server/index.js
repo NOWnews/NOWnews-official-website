55c8aff32be1efd75af3a5b3d7d82c46335ceb94
esbb48 <esbb48@gmail.com>
2017-06-20 03:22:20 +0800

    ✨ Feat: Remove no need code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ import throng from 'throng';
 import url from 'url';
 import isomorphicCookie from 'isomorphic-cookie';
 import axios from 'axios';
-import cors from 'cors';
 
 import React from 'react';
 import ReactDOM from 'react-dom/server';
