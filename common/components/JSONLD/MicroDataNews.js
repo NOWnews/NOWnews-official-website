3944116d7eee3602053b6e15ba6e2efda8bf7fec
wayne1025 <wayne1025.tw@hotmail.com>
2017-06-21 13:18:44 +0800

    ✨ Feat: add jsonld data

--- common/components/JSONLD/MicroDataNews.js
+++ common/components/JSONLD/MicroDataNews.js
@@ -5,23 +5,22 @@ const MicroDataNews = ({ news }) => {
   const data = {
     "@context": "http://schema.org",
 	  "@type": "NewsArticle",
-	  "datePublished": "2017-06-20T17:43+08:00",
-	  "dateModified": "2017-06-20T17:43+08:00",
+	  "datePublished": news.startedAt,
+	  "dateModified": news.startedAt,
 	  "mainEntityOfPage":{
 	    "@type":"WebPage",
-	    "@id":"http://www.nownews.com/n/2017/06/20/2571948"
+	    "@id": `http://www.nownews.com${news.parseUrl}`
 	  },
-	  "articleBody": "22歲陳姓女模在今年3月遭姦殺並棄屍在南港的廢棄商場，而凶手正是陳女閨密的男友程宇，引發譁然。當時程宇宣稱和女友梁女一起計畫犯案，過程中梁女更將包包背帶綁住陳女的脖子，而梁女也遭羈押3天。經檢警偵辦後",
-	  "headline": "梁女遭誣陷白關3天　檢方列6大原因不起訴",
+	  "articleBody": news.content,
 	  "image": {
 	    "@type": "ImageObject",
-	    "url": "http://imgapi.nownews.com/?w=640&q=60&src=http%3A%2F%2Fs.nownews.com%2F3e%2F01%2F3e01a26cadaa0a379e492906eb177b75.jpg",
+	    "url": news.MainPhoto.url,
 	    "width": 696,
 	    "height": 530
 	  },
 	  "author": {
 	    "@type": "Person",
-	    "name": "社會中心／台北報導"
+	    "name": news.newsBy
 	  },
 	   "publisher": {
 	    "@type": "Organization",
@@ -33,12 +32,11 @@ const MicroDataNews = ({ news }) => {
 	      "height": 52
 	    }
 	  },
-	  "description": "24歲陳姓女模在今年3月遭姦殺並棄屍在南港的廢棄商場，而凶手正是陳女閨密的男友程宇，引發譁然。當時程宇宣稱和女友梁女一起計畫犯案，過程中梁女更將包包背帶綁住陳女的脖子，而梁女也遭羈押3天。經檢警偵辦後"
+	  "description": news.summary
   };
   return (
-    <script type='application/ld+json'>
-      {`${JSON.stringify(data)}`}
-    </script>
+    <script type='application/ld+json'
+      dangerouslySetInnerHTML={{__html: JSON.stringify(data)}} />
   );
 };
 
