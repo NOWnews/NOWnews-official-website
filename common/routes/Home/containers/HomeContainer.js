fa0922a3d68c3a01a21cfc4752a4e8926f55bab5
esbb48 <esbb48@gmail.com>
2017-06-23 02:49:51 +0800

    ✨ Feat: Add grabBag
    167

--- common/routes/Home/containers/HomeContainer.js
+++ common/routes/Home/containers/HomeContainer.js
@@ -9,7 +9,7 @@ import { BlockItems9, BlockItems6, BlockItems8, SlideRight, VideoBlock } from '.
 import { Container, RightSide, LeftSide, Loading, Margin10 } from '../../../components/Layout';
 import { Slide } from '../../../components/News';
 import { AppleStyle, AndroidStyle } from '../../../components/AppBlock';
-import { DFP } from '../../../components/Ad';
+import { DFP, GrabBag } from '../../../components/Ad';
 
 import { loadHeader, selectMarquee, selectMenus } from '../../../modules/header';
 import { selectLBS, loadLBSList } from '../../../modules/LBS';
@@ -101,7 +101,7 @@ class HomeContainer extends Component {
                 </div>
               </LeftSide>
               <RightSide>
-
+                <GrabBag />
                 <Margin10>
                   <DFP opts={['/5799246/Nownews_home_300x250_M1_new2', [300, 250], 'div-gpt-ad-1496983171426-0']} />
                 </Margin10>
