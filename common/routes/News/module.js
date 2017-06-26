d0f9c4003c26f95782fbbc1739c7ee0dc8a9b10e
esbb48 <esbb48@gmail.com>
2017-06-26 20:21:27 +0800

    ✨ Feat: TrackFbContent

--- common/routes/News/module.js
+++ common/routes/News/module.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import isomorphicCookie from 'isomorphic-cookie';
-import { callApi as callPVApi } from '../../../lib/track/pageview';
+import { trackFBViewContent, callApi as callPVApi } from '../../../lib/track/pageview';
 export const CHANGE_FONT_SIZE = 'CHANGE_FONT_SIZE';
 export const CHANGE_NEWS_TITLE = 'CHANGE_NEWS_TITLE';
 export const LOAD_NEWS_REQUEST = 'LOAD_NEWS_REQUEST';
@@ -116,6 +116,7 @@ export const loadMoreNews = (sn) => {
       const { id: newsId, MainMenu, sn, startedAt } = result;
       const formatStartedAt = moment(startedAt).format('YYYYMMDD');
       callPVApi(apiServ, MainMenu.id, newsId, `/news/${formatStartedAt}/${sn}`, search, headers);
+      trackFBViewContent(result);
     }).catch(error => {
       console.error(`Error in reducer that handles ${LOAD_NEWS_FAILURE}: `, error);
       dispatch({
