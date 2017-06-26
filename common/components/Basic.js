50fa9b6c683b83b162e081a773b6d5ffc1717473
esbb48 <esbb48@gmail.com>
2017-06-26 19:31:06 +0800

    ✨ Feat: Add comscore

--- common/components/Basic.js
+++ common/components/Basic.js
@@ -43,6 +43,18 @@ class Basic extends PureComponent {
                 ga('send', 'pageview');
                 ga('26.send', 'pageview');
               `}} />
+            <script dangerouslySetInnerHTML={{__html: `
+                var _comscore = _comscore || [];
+                _comscore.push({ c1: "2", c2: "11473067" });
+                (function() {
+                  var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
+                  s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
+                  el.parentNode.insertBefore(s, el);
+                })();
+              `}} />
+            <noscript dangerouslySetInnerHTML={{__html: `
+                <img src='http://b.scorecardresearch.com/p?c1=2&c2=11473067&cv=2.0&cj=1' />
+              `}} />
           </div>
         </StaticContainer>
         {/* 目前先不放 <Idle /> */}
