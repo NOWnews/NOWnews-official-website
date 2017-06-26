45b1fa94e55dde45baaef9d1188938f7304f035c
wayne1025 <wayne1025.tw@hotmail.com>
2017-06-26 13:10:09 +0800

    ✨ Feat: test ga code without gtm

--- common/components/Basic.js
+++ common/components/Basic.js
@@ -2,7 +2,6 @@ import React, { PureComponent, PropTypes } from 'react';
 import { StyleSheet, css } from 'aphrodite/no-important';
 import Helmet from 'react-helmet';
 import StaticContainer from 'react-static-container';
-import { GTM } from './Tracking';
 // import Idle from './Idle';
 
 // 有要測試 Perf 的時候才打開，chrome 記得安裝相關套件
@@ -31,7 +30,17 @@ class Basic extends PureComponent {
                 {property: 'fb:app_id', content: 132863386747341},
                 {property: 'fb:pages', content: 102884532662}
               ]} />
-            <GTM gtmId='GTM-W25KLJG' />
+            <script dangerouslySetInnerHTML={{__html: `
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-4021556-54', 'auto');
+                ga('create', 'UA-4021556-26', {'name':'26'});
+                ga('send', 'pageview');
+                ga('26.send', 'pageview');
+              `}} />
           </div>
         </StaticContainer>
         {/* 目前先不放 <Idle /> */}
