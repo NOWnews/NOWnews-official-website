c09362d3f4501d6ef89c1b82695a186496d6911a
esbb48 <esbb48@gmail.com>
2017-06-26 11:32:43 +0800

    🐛 Fix: prevent img is null

--- common/components/News/ContentForNews.js
+++ common/components/News/ContentForNews.js
@@ -33,7 +33,7 @@ class ContentForNews extends PureComponent {
     return (
       <Container>
         <div className={css(styles.contentDiv)}>
-          <img className={css(styles.contentImg)} src={imgUrl} alt={news.MainPhoto.desc} />
+          <img className={css(styles.contentImg)} src={imgUrl} alt={news.MainPhoto && news.MainPhoto.desc} />
         </div>
         <i>{news.MainPhoto && news.MainPhoto.desc}</i>
         <Margin10 className='clearfix'>
