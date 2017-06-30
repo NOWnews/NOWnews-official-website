d49e0d135b78b6b62e16076136bf0148fde2cd38
esbb48 <esbb48@gmail.com>
2017-06-30 15:43:56 +0800

    ✨ Feat: Update footer link
    109

--- common/components/Footer/Footer.js
+++ common/components/Footer/Footer.js
@@ -8,12 +8,12 @@ export const Footer = ({ ads }) => {
   const moreList = [
     { isExternal: false, title: '立院直播', url: '/lylive/1' },
     { isExternal: true, title: '購屋通', url: 'http://www.cthouse.com.tw/?utm_source=nownews&utm_medium=text&utm_content=navigation&utm_campaign=nownews' },
-    // { isExternal: false, title: '名家論壇', url: '' },
+    { isExternal: false, title: '名家論壇', url: '/cat/celebritycomment' },
     { isExternal: true, title: '飆網超省', url: 'https://www.twmbroadband.com/T01/cm-promo-more-2_3_0_1053_15.html?&mkwid=SjDd8oD' },
-    // { isExternal: false, title: '公益行善', url: '' },
+    { isExternal: false, title: '公益行善', url: '/cat/public' },
     { isExternal: true, title: '網頁遊戲', url: 'http://nownews.nicegame.com.tw/' },
-    { isExternal: true, title: '投票區', url: 'http://vote.nownews.com/' }
-    // { isExternal: false, title: '色區', url: '' }
+    { isExternal: true, title: '投票區', url: 'http://vote.nownews.com/' },
+    { isExternal: false, title: '色區', url: '/cat/hotzone' }
   ];
   const socialList = [
     { icon: 'FB', url: 'https://facebook.com/nownews' },
