167c32fb71a01cb00727559e7c2a172cb31d984e
esbb48 <esbb48@gmail.com>
2017-07-04 19:21:10 +0800

    ✨ Feat: Add tracking for SSR

--- common/components/Basic.js
+++ common/components/Basic.js
@@ -2,17 +2,30 @@ import React, { PureComponent, PropTypes } from 'react';
 import { StyleSheet, css } from 'aphrodite/no-important';
 import Helmet from 'react-helmet';
 import { GTM } from './Tracking';
+import { connect } from 'react-redux';
 import StaticContainer from 'react-static-container';
+import { init } from '../../lib/track/pageview';
+
 // import Idle from './Idle';
 
 // 有要測試 Perf 的時候才打開，chrome 記得安裝相關套件
 // import Perf from 'react-addons-perf';
 
+const mapStateToProps = state => ({
+  state
+});
+
 class Basic extends PureComponent {
-  // 有要測試 Perf 的時候才打開，chrome 記得安裝相關套件
-  // componentDidMount () {
-  //   window.Perf = Perf;
-  // }
+  componentDidMount () {
+    // 有要測試 Perf 的時候才打開，chrome 記得安裝相關套件
+    // window.Perf = Perf;
+
+    // For first time SSR
+    const state = this.props.state;
+    const { apiServ, headers } = state.sourceRequest;
+    const { pathname, search } = window.location;
+    init(apiServ, pathname, search, state, headers);
+  }
 
   render () {
     return (
@@ -72,6 +85,7 @@ const styles = StyleSheet.create({
 });
 
 Basic.propTypes = {
-  children: PropTypes.any.isRequired
+  children: PropTypes.any.isRequired,
+  state: PropTypes.object
 };
-export default Basic;
+export default connect(mapStateToProps)(Basic);
