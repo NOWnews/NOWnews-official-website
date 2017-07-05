3da3f990338dbe9fb1b6d5104b144cd6e4d9a50f
esbb48 <esbb48@gmail.com>
2017-07-06 01:31:49 +0800

    🐛 Fix: LBS page can not fetch data

--- common/modules/LBS.js
+++ common/modules/LBS.js
@@ -1,10 +1,12 @@
 export const LOAD_LBS_REQUEST = 'LOAD_LBS_REQUEST';
 export const LOAD_LBS_SUCCESS = 'LOAD_LBS_SUCCESS';
 export const LOAD_LBS_FAILURE = 'LOAD_LBS_FAILURE';
+export const LOAD_LOCATION_REQUEST = 'LOAD_LOCATION_REQUEST';
 
 const initialState = {
   error: null,
   isLoading: false,
+  isLocationLoading: false,
   lastFetched: null,
   location: [],
   mapCity: '',
@@ -28,18 +30,26 @@ function getLocation () {
   });
 }
 
-export function loadLBSList (page = 1) {
+export function loadLBSList () {
   return (dispatch, getState, { axios }) => {
-    // 有資料同一頁就跳過，不重複呼叫
-    const { newsList, pageData } = getState().LBS;
-    if (newsList.length > 0 && pageData.currentPage === page.toString()) {
+    // skip SSR, because it should work on client side!.
+    const canUseDOM = !!(typeof window !== 'undefined' && window.document);
+    if (!canUseDOM) {
       return Promise.resolve();
     }
 
+    // 有抓過資料就跳過，不重複呼叫
+    const { apiServ, local } = getState().sourceRequest;
+    const { lastFetched, pageData } = getState().LBS;
+    const page = local.query.page ? local.query.page : 1;
+    if (lastFetched !== null && pageData.currentPage === parseInt(page, 10)) {
+      return Promise.resolve();
+    }
+    dispatch({ type: LOAD_LOCATION_REQUEST });
     getLocation().then((location) => {
       const { latitude: lat, longitude: lng } = location.coords;
-      const { apiServ } = getState().sourceRequest;
       dispatch({ type: LOAD_LBS_REQUEST, payload: [lat, lng] });
+
       return axios.get(`${apiServ}/location?limit=10&lat=${lat}&lng=${lng}&page=${page}`)
       .then(res => {
         dispatch({
@@ -68,6 +78,7 @@ export default function LBS (state = initialState, action) {
         location: action.payload,
         mapCity: '',
         newsList: [],
+        isLocationLoading: false,
         isLoading: true,
         error: null
       };
@@ -86,6 +97,12 @@ export default function LBS (state = initialState, action) {
         ...state,
         error: action.payload.message,
         isLoading: false,
+        newsList: []
+      };
+    case LOAD_LOCATION_REQUEST:
+      return {
+        ...state,
+        isLocationLoading: true,
         newsList: []
       };
     default:
