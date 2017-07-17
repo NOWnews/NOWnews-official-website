bbc66249994a116dc0c7faf3e597b47f05041c9a
esbb48 <esbb48@gmail.com>
2017-07-17 11:57:41 +0800

    🐛 Fix: Fix preview-image for line

--- common/routes/News/containers/NewsContainer.js
+++ common/routes/News/containers/NewsContainer.js
@@ -191,6 +191,8 @@ class NewsContainer extends PureComponent {
         metaOpts.push({ name: 'twitter:card', content: newsMainPhoto });
         metaOpts.push({ property: 'og:image', content: newsMainPhoto });
         metaOpts.push({ property: 'popin:image', content: newsMainPhoto });
+        // 為了讓 line 可以爬到圖片（縮圖會爬不到）
+        metaOpts.push({ property: 'og:image', content: news.MainPhoto.url });
       }
 
       if (news.type === 'VIDEO') {
