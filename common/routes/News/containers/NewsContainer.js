a592d5507f66c2a5b3b18b84418525730eeb8ede
esbb48 <esbb48@gmail.com>
2017-08-01 11:37:24 +0800

    ✨ Feat: Remove OneAdICIP

--- common/routes/News/containers/NewsContainer.js
+++ common/routes/News/containers/NewsContainer.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import InfiniteScroll from 'react-infinite-scroller';
 import { FixedHeader, Header } from '../../../components/Header';
-import { DFP, getAdType, OneAdICIP } from '../../../components/Ad';
+import { DFP, getAdType } from '../../../components/Ad';
 import { Container, NotFound } from '../../../components/Layout';
 import { Head, ContentForNews, ContentForPhoto, ContentForVideo, ContentForCustomColumn } from '../../../components/News';
 import {
@@ -236,7 +236,6 @@ class NewsContainer extends PureComponent {
         <Header ad={topAd} menus={menus} marquee={marquee}
           currentChildMenu={childMenuId}
           currentMainMenu={mainMenuId} />
-        {isDefaultTemplate && <OneAdICIP />}
         {showFixedHeader && <FixedHeader menus={this.props.menus}
           currentMainMenu={mainMenuId} newsTitle={newsTitle} />}
         {!isLoading && !news && <Container><NotFound /></Container>}
