2d1b77fa2a2565f312a973df51e2b33b49c7b8f7
esbb48 <esbb48@gmail.com>
2017-08-11 18:54:17 +0800

    ✨ Feat: Finish

--- common/components/Ad/CTHouse.js
+++ common/components/Ad/CTHouse.js
@@ -5,7 +5,7 @@ import { Margin10 } from '../Layout';
 // 中信房屋的廣告
 const CTHouse = () => (
   <Margin10>
-    <DFP opts={['/5799246/Nownews_home_300x250_M3_new2', [300, 250], 'div-gpt-ad-1496983228493-0']} />
+    <DFP opts={['/5799246/Nownews_home_300x250_M3_new2', [300, 250]]} />
   </Margin10>
 );
 
