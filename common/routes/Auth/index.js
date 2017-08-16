1e3d5ece8f710261204db48d65119f1c79e2756c
esbb48 <esbb48@gmail.com>
2017-08-17 02:18:50 +0800

    🔨 Refactor: Hidden no use view

--- common/routes/Auth/index.js
+++ common/routes/Auth/index.js
@@ -11,39 +11,39 @@ export default function createRoutes (store) {
         const authFormReducer = require('./module').default;
         injectAsyncReducer(store, 'authPage', authFormReducer);
         cb(null, [
-          {
-            path: 'active',
-            getComponents (location, cb) {
-              require.ensure([
-                './containers/ActiveContainer'
-              ], (require) => {
-                const ActiveContainer = require('./containers/ActiveContainer').default;
-                cb(null, ActiveContainer);
-              }, 'activePage');
-            }
-          },
-          {
-            path: 'forgot',
-            getComponents (location, cb) {
-              require.ensure([
-                './containers/ForgotContainer'
-              ], (require) => {
-                const ForgotContainer = require('./containers/ForgotContainer').default;
-                cb(null, ForgotContainer);
-              }, 'fotgotPage');
-            }
-          },
-          {
-            path: 'login',
-            getComponents (location, cb) {
-              require.ensure([
-                './containers/LoginContainer'
-              ], (require) => {
-                const LoginContainer = require('./containers/LoginContainer').default;
-                cb(null, LoginContainer);
-              }, 'loginPage');
-            }
-          },
+          // {
+          //   path: 'active',
+          //   getComponents (location, cb) {
+          //     require.ensure([
+          //       './containers/ActiveContainer'
+          //     ], (require) => {
+          //       const ActiveContainer = require('./containers/ActiveContainer').default;
+          //       cb(null, ActiveContainer);
+          //     }, 'activePage');
+          //   }
+          // },
+          // {
+          //   path: 'forgot',
+          //   getComponents (location, cb) {
+          //     require.ensure([
+          //       './containers/ForgotContainer'
+          //     ], (require) => {
+          //       const ForgotContainer = require('./containers/ForgotContainer').default;
+          //       cb(null, ForgotContainer);
+          //     }, 'fotgotPage');
+          //   }
+          // },
+          // {
+          //   path: 'login',
+          //   getComponents (location, cb) {
+          //     require.ensure([
+          //       './containers/LoginContainer'
+          //     ], (require) => {
+          //       const LoginContainer = require('./containers/LoginContainer').default;
+          //       cb(null, LoginContainer);
+          //     }, 'loginPage');
+          //   }
+          // },
           {
             path: 'me',
             getComponents (location, cb) {
@@ -65,17 +65,17 @@ export default function createRoutes (store) {
                 cb(null, OauthContainer);
               }, 'oauthPage');
             }
-          },
-          {
-            path: 'signup',
-            getComponents (location, cb) {
-              require.ensure([
-                './containers/SignupContainer'
-              ], (require) => {
-                const SignupContainer = require('./containers/SignupContainer').default;
-                cb(null, SignupContainer);
-              }, 'signupPage');
-            }
+          // },
+          // {
+          //   path: 'signup',
+          //   getComponents (location, cb) {
+          //     require.ensure([
+          //       './containers/SignupContainer'
+          //     ], (require) => {
+          //       const SignupContainer = require('./containers/SignupContainer').default;
+          //       cb(null, SignupContainer);
+          //     }, 'signupPage');
+          //   }
           }
         ]);
       });
