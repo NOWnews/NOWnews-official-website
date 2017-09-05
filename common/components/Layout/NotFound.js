c87ec53eec6315df603b182152ea5fc3a3b6f70a
esbb48 <esbb48@gmail.com>
2017-09-05 18:12:01 +0800

    ✨ Feat: Add special Id for notFound

--- common/components/Layout/NotFound.js
+++ common/components/Layout/NotFound.js
@@ -3,12 +3,12 @@ import Link from 'react-router/lib/Link';
 import { StyleSheet, css } from 'aphrodite/no-important';
 
 const NotFound = () => (
-  <div className='center'>
+  <div id='NotFound' className='center'>
     <img className={css(styles.notFoundImg)} src='/404.png' />
     <h1>找不到網頁或內容，回<Link to='/'>首頁</Link>看看吧！</h1>
-
   </div>
 );
+
 
 const styles = StyleSheet.create({
   notFoundImg: {
