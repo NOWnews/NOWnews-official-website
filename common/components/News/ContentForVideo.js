b8b7b9d088a00ffa9275b4c86354cd63122d416f
esbb48 <esbb48@gmail.com>
2017-09-08 11:35:39 +0800

    🐛 Fix: Add Tags for NewsForContent

--- common/components/News/ContentForVideo.js
+++ common/components/News/ContentForVideo.js
@@ -37,6 +37,7 @@ class ContentForVideo extends PureComponent {
         <Margin10 className='clearfix'>
           <LeftSide>
             <Content content={news.content} fontSize={fontSize} freeContent={news.freeContent} />
+            <Tags tags={news.Tags || []} />
             {/* TODO: UNA 廣告延後 先隱藏 <Margin10>
               <DFP opts={[`/5799246/Nownews_${adType}_article_600x225_i_new2`, [600, 225]]} />
             </Margin10> */}
