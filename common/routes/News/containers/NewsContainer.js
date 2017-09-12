546f5fcf0ff6902951dc66b1ea4a65e23309f458
esbb48 <esbb48@gmail.com>
2017-09-12 14:31:55 +0800

    ✨ Feat: Update originSource

--- common/routes/News/containers/NewsContainer.js
+++ common/routes/News/containers/NewsContainer.js
@@ -172,7 +172,7 @@ class NewsContainer extends PureComponent {
       const MainPhoto = news.MainPhoto;
       metaOpts.push({ name: 'twitter:image', content: MainPhoto.medium });
       metaOpts.push({ name: 'twitter:card', content: MainPhoto.medium });
-      metaOpts.push({ property: 'og:image', content: MainPhoto.url });
+      metaOpts.push({ property: 'og:image', content: MainPhoto.originSource });
       metaOpts.push({ property: 'popin:image', content: MainPhoto.url });
 
       if (news.type === 'VIDEO') {
