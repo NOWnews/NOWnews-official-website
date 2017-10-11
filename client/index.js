6f08884e4607325cee70cfada992bf43b205cf02
esbb48 <esbb48@gmail.com>
2017-10-11 16:49:58 +0800

    🐛 Fix: Support different browser

--- client/index.js
+++ client/index.js
@@ -73,7 +73,8 @@ const render = () => {
         // Fetch mandatory data dependencies for 2nd route change onwards:
         trigger('fetch', components, locals)
         .then(() => {
-          window.document.documentElement.scrollTop = 0;
+          window.document.documentElement.scrollTop = 0; // For chrome, IE
+          window.document.body.scrollTop = 0; // For Safari, Edge
           const { pathname, search } = renderProps.location;
           const { sourceRequest: { apiServ, headers }, ...state } = store.getState();
           pageview.spaInit(apiServ, pathname, search, state, headers);
