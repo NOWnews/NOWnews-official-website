fd2f168577d1568516041a32eb2e7015ac69f1d6
Webber <bird23074035@gmail.com>
2017-11-01 14:28:41 +0800

    🎨 Style: idle 樣式

--- common/components/Idle/Idle.js
+++ common/components/Idle/Idle.js
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react';
 import Modal from 'react-overlays/lib/Modal';
 import { StyleSheet, css } from 'aphrodite/no-important';
+import { DFP } from '../Ad';
+
+// import { selectInterest, loadInterest } from '../../../modules/interest';
 
 class Idle extends PureComponent {
   constructor (props) {
@@ -45,7 +48,7 @@ class Idle extends PureComponent {
   }
 
   scrollListener () {
-    const timer = 30 * 1000;
+    const timer = 1 * 1000;
     if (this.idleInterval) {
       window.clearInterval(this.idleInterval);
     }
@@ -57,6 +60,9 @@ class Idle extends PureComponent {
   }
 
   render () {
+    const ad1 = '/5799246/Nownews_home_970x250_T_new2';
+    const ad2 = '/5799246/Nownews_all_article_300x250_artm';
+
     return (
       <Modal
         aria-labelledby='modal-label'
@@ -66,7 +72,61 @@ class Idle extends PureComponent {
         onHide={this.close}>
         <div className={css(styles.dialog)}>
           <span className={css(styles.closeBtn)} onClick={this.close}>✖ 關閉</span>
-          <h1>閒置頁面</h1>
+          <div>
+            <div className={css(styles.dialogTitle)}>本頁面已經閒置超過10分鐘，您可以點擊空白處回原網址，NOWnews關心您</div>
+            <div className={css(styles.dialogContent)}>
+              <div>
+                <a href='/' className={css(styles.newsBlock)}>
+                  <img className={css(styles.newsImg)} src='/subWebBanner_colorful/pinknow.jpg' />
+                  <h3>本頁面已經閒置超過10分鐘，您可以點擊空白處回原網址</h3>
+                </a>
+                <a href='/' className={css(styles.newsBlock)}>
+                  <img className={css(styles.newsImg)} src='/subWebBanner_colorful/pinknow.jpg' />
+                  <h3>本頁面已經閒置超過10分鐘，您可以點擊空白處回原網址</h3>
+                </a>
+                <a href='/' className={css(styles.newsBlock)}>
+                  <img className={css(styles.newsImg)} src='/subWebBanner_colorful/pinknow.jpg' />
+                  <h3>本頁面已經閒置超過10分鐘，您可以點擊空白處回原網址</h3>
+                </a>
+              </div>
+              <div className={css(styles.clearfix)} />
+              <div className={css(styles.dfp970x50)}>
+                {ad1 && <DFP opts={[ad1, [[970, 90], [728, 90]]]} />}
+              </div>
+              <div>
+                <div className={css(styles.listNews)}>
+                  <a href='/news/20171101/2635686' className={css(styles.newsLink)}>
+                    <span className={css(styles.newsMenu)}>財經</span>
+                    <span>蘋果股價創高　台股再戰10800</span>
+                  </a>
+                  <a href='/news/20171101/2635686' className={css(styles.newsLink)}>
+                    <span className={css(styles.newsMenu)}>財經</span>
+                    <span>蘋果股價創高　台股再戰10800</span>
+                  </a>
+                  <a href='/news/20171101/2635686' className={css(styles.newsLink)}>
+                    <span className={css(styles.newsMenu)}>財經</span>
+                    <span>蘋果股價創高　台股再戰10800</span>
+                  </a>
+                  <a href='/news/20171101/2635686' className={css(styles.newsLink)}>
+                    <span className={css(styles.newsMenu)}>財經</span>
+                    <span>蘋果股價創高　台股再戰10800</span>
+                  </a>
+                  <a href='/news/20171101/2635686' className={css(styles.newsLink)}>
+                    <span className={css(styles.newsMenu)}>財經</span>
+                    <span>蘋果股價創高　台股再戰10800</span>
+                  </a>
+                  <a href='/news/20171101/2635686' className={css(styles.newsLink)}>
+                    <span className={css(styles.newsMenu)}>財經</span>
+                    <span>蘋果股價創高　台股再戰10800</span>
+                  </a>
+                </div>
+                <div className={css(styles.dfp300x250)}>
+                  {ad2 && <DFP opts={[ad2, [[300, 250], [336, 280]]]} />}
+                </div>
+                <div className={css(styles.clearfix)} />
+              </div>
+            </div>
+          </div>
         </div>
       </Modal>
     );
@@ -109,14 +169,64 @@ const styles = StyleSheet.create({
   dialog: {
     textAlign: 'center',
     position: 'absolute',
-    width: 400,
+    width: 1000,
     top: `${top}%`,
     left: `${left}%`,
     transform: `translate(-${top}%, -${left}%)`,
     border: '1px solid #e5e5e5',
     backgroundColor: 'white',
-    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
-    padding: 20
+    boxShadow: '0 5px 15px rgba(0,0,0,.5)'
+    // display: 'none'
+  },
+  dialogTitle: {
+    backgroundColor: '#fec340',
+    padding: 16
+  },
+  dialogContent: {
+    paddingLeft: 15
+  },
+  newsBlock: {
+    display: 'block',
+    float: 'left',
+    width: '300px',
+    margin: '20px 12px 0'
+  },
+  newsImg: {
+    width: 300,
+    height: 180
+  },
+  dfp970x50: {
+    width: 970,
+    margin: '17px 0'
+  },
+  dfp300x250: {
+    width: 300,
+    float: 'left'
+  },
+  listNews: {
+    width: '665px',
+    float: 'left',
+    paddingBottom: '20px'
+  },
+  newsLink: {
+    display: 'block',
+    textAlign: 'left',
+    borderTop: '1px solid #a8a9aa',
+    padding: '9px 0',
+    margin: '0 20px',
+    color: '#000',
+    textDecoration: 'none',
+    ':last-child': {
+      borderBottom: '1px solid #A8A9AA'
+    }
+  },
+  newsMenu: {
+    backgroundColor: '#fec340',
+    padding: '7px 12px',
+    marginRight: '15px'
+  },
+  clearfix: {
+    clear: 'both'
   }
 });
 
