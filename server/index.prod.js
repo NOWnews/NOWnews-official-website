c78095e98ab42029bc4feb4a826b537931fc0bb0
esbb48 <esbb48@gmail.com>
2017-11-14 11:53:07 +0800

    ✨ Feat: Init autotrack config and js

--- server/index.prod.js
+++ server/index.prod.js
@@ -144,6 +144,10 @@ export const createServer = (config) => {
                     ga('send', 'pageview');
                     ga('26.send', 'pageview');
                   }
+                  ga('require', 'eventTracker', {
+                    attributePrefix: 'data-',
+                    events: ['click', 'auxclick', 'contextmenu']
+                  });
                   var _comscore = _comscore || [];
                   _comscore.push({ c1: "2", c2: "11473067" });
                   (function() {
@@ -155,6 +159,7 @@ export const createServer = (config) => {
                 <noscript>
                   <img src='http://b.scorecardresearch.com/p?c1=2&c2=11473067&cv=2.0&cj=1' />
                 </noscript>
+                <script src="/vendor/autotrack-eventTracker.min.js"></script>
                 <script type="text/javascript">
                   (function() {
                     var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.charset = "utf-8"; pa.async = true;
