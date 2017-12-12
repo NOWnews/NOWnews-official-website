fb505cc5521b3a418a3336fe66bcf434dc43de07
Webber <bird23074035@gmail.com>
2017-12-12 18:12:21 +0800

    ✨ Feat: 加上onclick 事件

--- common/routes/Home/components/BodyColor.js
+++ common/routes/Home/components/BodyColor.js
@@ -14,12 +14,19 @@ class BodyColor extends React.Component {
     body[0].style.backgroundRepeat = 'no-repeat';
     body[0].style.backgroundSize = '100%';
     body[0].style.backgroundAttachment = 'fixed';
+    body[0].onclick = (event) => {
+      let parentDom = event.target.parentElement.getAttribute('class');
+      if (parentDom.indexOf('root_') > -1) {
+        window.open('https://www.google.com', '_blank');
+      }
+    };
   }
 //   componentWillReceiveProps (nextProps) {
 //   }
   componentWillUnmount () {
     var body = document.getElementsByTagName('body');
     body[0].style.background = `#FFFFFF`;
+    body[0].onclick = null;
   }
   render () {
     return null;
