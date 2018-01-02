39ba6f37a2e3d2da9e18cb5908c3c6f867587103
esbb48 <esbb48@gmail.com>
2018-01-02 18:36:46 +0800

    ✨ Feat: Add mediafarmer

---
+++ common/components/News/NewsContent/MediaFarmer.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+
+const MediaFarmer = ({ news }) => (
+  <div dangerouslySetInnerHTML={{__html: `
+    <div id='herbsapi'
+      hb-author='${news.newsBy}'
+      hb-title='${news.title}'
+      hb-width='650'
+      hb-height='92'
+      hb-icon='/others/media-farmer.png' />
+  `}} />
+);
+
+MediaFarmer.propTypes = {
+  news: PropTypes.object.isRequired
+};
+export default MediaFarmer;
--- common/components/News/NewsContent/index.js
+++ common/components/News/NewsContent/index.js
@@ -1,5 +1,6 @@
 export FontSize from './FontSize';
 export Content from './Content';
+export MediaFarmer from './MediaFarmer';
 export PrevAndNext from './PrevAndNext';
 export RecommendAds from './RecommendAds';
 export RelatedContent from './RelatedContent';
