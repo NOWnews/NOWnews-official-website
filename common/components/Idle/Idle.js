1888c1452818f023a24f5195b685b953ea594179
LiweiTW <vincent.cdw@gmail.com>
2018-04-09 22:01:28 +0800

    Not gonna work cuz "Same-origin policy"...

--- common/components/Idle/Idle.js
+++ common/components/Idle/Idle.js
@@ -32,7 +32,6 @@ class Idle extends PureComponent {
     scrollEl.addEventListener('mousemove', this.scrollListener);
     scrollEl.addEventListener('resize', this.scrollListener);
     scrollEl.addEventListener('scroll', this.scrollListener);
-    scrollEl.top.addEventListener('keydown', this.scrollListener);
   }
 
   close = () => {
@@ -45,7 +44,6 @@ class Idle extends PureComponent {
     scrollEl.removeEventListener('mousemove', this.scrollListener);
     scrollEl.removeEventListener('resize', this.scrollListener);
     scrollEl.removeEventListener('scroll', this.scrollListener);
-    scrollEl.top.removeEventListener('keydown', this.scrollListener);
   }
 
   scrollListener () {
