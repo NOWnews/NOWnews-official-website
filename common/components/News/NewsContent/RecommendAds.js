5a16a4db8dadb7d649eed65118b99f8b09de375a
LiWei <Vincent.cdw@gmail.com>
2018-08-14 14:24:35 +0800

    Update RecommendAds.js

--- common/components/News/NewsContent/RecommendAds.js
+++ common/components/News/NewsContent/RecommendAds.js
@@ -7,7 +7,7 @@ const RecommendAds = ({ ads }) => {
 
   ads.map((ad, index) => {
     // gif file check format.
-    let adImgFormat = {ad.img};
+    let adImgFormat = ad.img;
 
     if (ad.img.indexOf('.gif') > 0) {
       adImgFormat = ad.img;
